8c4e81296c56e647f493b08949e10aff867a079f
moonl <347755658>
2019-10-09 17:35:58 +0800

    add

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -6,17 +6,48 @@ use tools\Sysinfo;
 use think\Db;
 class Order extends Base
 {
-    protected $dataform ='';
+    protected $dataform ='order';
 
 
     //未完成
     public function wei(){
 
+        if($this->request->isGet()){
+          $list = Db::name($this->dataform)->where('status',0)->paginate(15);
+          $this->assign('list',$list);
+          return $this->fetch();
+        }
+
+
+
+    }
+
+
+
+    public function edit(){
+        if($this->request->isGet()){
+
+            return $this->fetch();
+        }
     }
 
     //已完成
     public function over(){
+        if($this->request->isGet()){
+            $list = Db::name($this->dataform)->where('status',1)->paginate(15);
+            $this->assign('list',$list);
+            return $this->fetch();
+        }
+    }
 
+
+    //已取消
+    public function nomal(){
+        if($this->request->isGet()){
+            $list = Db::name($this->dataform)->where('status',-1)->paginate(15);
+            $this->assign('list',$list);
+            return $this->fetch();
+        }
     }
 
 
---
+++ runtime/temp/5857eb40e47cf20f0541b836ac08b1c0.php
@@ -0,0 +1,331 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:109:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\duowan\public/../application/admin\view\order\nomal.html";i:1570613679;s:103:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\duowan\application\admin\view\template\layout.html";i:1569227915;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
+    <title>多玩后台管理</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta itemprop="image" content="/static/admin/images/touch-icon-iphone-retina.png">
+
+    <link rel="stylesheet" href="/static/admin/css/app.min.css">
+
+    <script>
+        var dataDelIds = [];
+    </script>
+    <script src="/static/admin/js/app.min.js"></script>
+    <script src="/static/admin/plugins/layer/layer.js"></script>
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php if(\think\Session::get('error_message')): ?>
+    <script>
+        layer.msg('<?php echo \think\Session::get('error_message'); ?>',{icon:2});
+    </script>
+    <?php endif; if(\think\Session::get('success_message')): ?>
+    <script>
+        layer.msg('<?php echo \think\Session::get('success_message'); ?>',{icon:1});
+    </script>
+    <?php endif; ?>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+    <header class="main-header">
+        <a class="logo">
+            <span class="logo-lg"><b>多玩后台管理</b></span>
+        </a>
+        <nav class="navbar navbar-static-top">
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">切换导航</span>
+            </a>
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="<?php echo $webData['user_info']['avatar']; ?>" class="user-image"
+                                 alt="User Image">
+                            <span class="hidden-xs"><?php echo $webData['user_info']['nickname']; ?></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="user-header">
+                                <img src="<?php echo $webData['user_info']['avatar']; ?>" class="img-circle"
+                                     alt="User Image">
+                                <p>
+                                    <?php echo $webData['user_info']['nickname']; ?>
+                                    <small><?php echo $webData['user_info']['name']; ?></small>
+                                </p>
+                            </li>
+                            <li class="user-body">
+                                <div class="row">
+                                    <div class="col-xs-4 text-center">
+                                        <a href="#"></a>
+                                    </div>
+                                    <div class="col-xs-4 text-center">
+                                        <a href="#"></a>
+                                    </div>
+                                    <div class="col-xs-4 text-center">
+                                        <a href="#"></a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="<?php echo url('admin_user/profile'); ?>" class="btn btn-default btn-flat">个人资料</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="<?php echo url('auth/logout'); ?>" class="btn btn-default btn-flat">退出</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a data-toggle="control-sidebar"><i class="fa fa-cog"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <aside class="main-sidebar">
+        <section class="sidebar">
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="<?php echo $webData['user_info']['avatar']; ?>" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p><?php if(!empty($user_info['name'])): ?><?php echo $user_info['name']; endif; ?></p>
+                    <a><i class="fa fa-circle text-success"></i> 正常</a>
+                </div>
+            </div>
+            <form method="get" class="sidebar-form" id="sidebar-form">
+                <div class="input-group">
+                    <input type="text" name="q" class="form-control" placeholder="搜索菜单" id="search-input">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+                </div>
+            </form>
+            <ul class="sidebar-menu">
+                <?php echo $webData['left_menu']; ?>
+            </ul>
+        </section>
+    </aside>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                <?php echo $webData['title']; ?>
+            </h1>
+            <ol class="breadcrumb">
+                <?php if(isset($webData['current_nav']) && !empty($webData['current_nav'])): ?>
+                <li><a href="<?php echo url('admin/index/index'); ?>"><i class="fa fa-home"></i> 主页</a></li>
+                <?php echo $webData['current_nav']; endif; ?>
+            </ol>
+        </section>
+        <section class="content">
+            <div class="row">
+
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+
+            <div class="box-body table-responsive">
+                <table class="table table-hover table-bordered datatable" width="100%">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>订单号</th>
+                        <th>游戏类型</th>
+                        <th>用户</th>
+                        <th>充值类型</th>
+                        <th>创建时间</th>
+                        <th>操作</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php if(is_array($list) || $list instanceof \think\Collection || $list instanceof \think\Paginator): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>
+                    <tr>
+                        <td><?php echo $vo['id']; ?></td>
+                        <td><?php echo $vo['orderId']; ?></td>
+                        <td><?php echo $vo['mid']; ?></td>
+                        <td><?php echo $vo['cid']; ?></td>
+                        <td><?php echo date('Y-m-d h:i',$vo['create_time']); ?></td>
+
+                        <td class="td-do">
+                            <a data-href="<?php echo url('order/edit',array('id'=>$vo['id'])); ?>"
+                               class="btn btn-primary btn-xs edit" title="修改">
+                                <i class="fa fa-pencil"></i>
+                            </a>
+                            <a class="btn btn-danger btn-xs del"
+                               title="删除"  data-url="<?php echo url('order/del',array('id'=>$vo['id'])); ?>">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <?php endforeach; endif; else: echo "" ;endif; ?>
+
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="box-footer">
+                <?php echo $list->render(); ?>
+                <label class="control-label pull-right" style="margin-right: 10px; font-weight: 100;">
+                    <?php if(isset($total)): ?>
+                    <small>共<?php echo $total; ?> 条记录</small>
+                    &nbsp;
+                    <?php endif; ?>
+                    <small>每页显示10条</small>
+                </label>
+            </div>
+
+
+        </div>
+
+        <script>
+
+            $('.edit').click(function(){
+                var url = $(this).attr('data-href');
+
+                layer.open({
+                    type: 2,
+                    title: '添加',
+                    area: ['50%', '60%'],
+                    anim: 2,
+                    content: url, //iframe的url，no代表不显示滚动条
+                })
+            })
+
+
+            $('.del').click(function(){
+
+                var url = $(this).attr('data-url');
+
+                layer.confirm('您是确定要删除？', {
+                    btn: ['确定','点错了'] //按钮
+                }, function(){
+                    $.get(url,function(ret){
+                        if(ret.code == 200){
+                            layer.msg(ret.msg,function(){
+                                parent.location.reload();
+                            })
+                        }
+
+                        if(ret.code == 400){
+                            layer.msg(ret.msg,function(){
+                                parent.location.reload();
+                            })
+                        }
+                    },'json')
+                }, function(){
+                    layer.close();
+                });
+
+            })
+
+        </script>
+
+        </section>
+    </div>
+
+    <!--删除操作modal-->
+    <div class="modal fade" id="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span></button>
+                    <h4 class="modal-title">提示</h4>
+                </div>
+                <div class="modal-body">
+                    <p id="modal_message">您确认执行本次操作吗？</p>
+                    <input name="modal_do_url" id="modal_do_url" value="" placeholder="modal_do_url" type="hidden">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">取消</button>
+                    <button onclick="modal_do()" type="button" class="btn btn-primary">确认</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>V</b> 1.0
+        </div>
+        <strong>Copyright &copy; 2018 <a></a>.</strong> All rights
+        reserved.
+    </footer>
+    <aside class="control-sidebar control-sidebar-dark">
+        <div class="tab-content">
+            <div class="tab-pane" id="control-sidebar-home-tab">
+            </div>
+        </div>
+    </aside>
+    <div class="control-sidebar-bg"></div>
+</div>
+</body>
+</html>
+<script src="/static/admin/js/demo.js"></script>
+<script>
+    $(function () {
+        $('#sidebar-form').on('submit', function (e) {
+            e.preventDefault();
+        });
+
+        $('.sidebar-menu li.active').data('lte.pushmenu.active', true);
+
+        $('#search-input').on('keyup', function () {
+            var term = $('#search-input').val().trim();
+
+            if (term.length === 0) {
+                $('.sidebar-menu li').each(function () {
+                    $(this).show(0);
+                    $(this).removeClass('active');
+                    if ($(this).data('lte.pushmenu.active')) {
+                        $(this).addClass('active');
+                    }
+                });
+                return;
+            }
+
+            $('.sidebar-menu li').each(function () {
+                if ($(this).text().toLowerCase().indexOf(term.toLowerCase()) === -1) {
+                    $(this).hide(0);
+                    $(this).removeClass('pushmenu-search-found', false);
+
+                    if ($(this).is('.treeview')) {
+                        $(this).removeClass('active');
+                    }
+                } else {
+                    $(this).show(0);
+                    $(this).addClass('pushmenu-search-found');
+
+                    if ($(this).is('.treeview')) {
+                        $(this).addClass('active');
+                    }
+
+                    var parent = $(this).parents('li').first();
+                    if (parent.is('.treeview')) {
+                        parent.show(0);
+                    }
+                }
+
+                if ($(this).is('.header')) {
+                    $(this).show();
+                }
+            });
+
+            $('.sidebar-menu li.pushmenu-search-found.treeview').each(function () {
+                $(this).find('.pushmenu-search-found').show(0);
+            });
+        });
+    });
+</script>
